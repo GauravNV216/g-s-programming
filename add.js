7e40d120ef1cb5cb91c5e17dbac303d39df16195
GauravNV216 <81035008+GauravNV216@users.noreply.github.com>
2021-03-20 21:48:19 +0530

    Add files via upload

---
+++ add.js
@@ -0,0 +1,11 @@
+// function myfn(){
+//     var y = document.getElementById("name").value;
+//     alert(y);}
+$(document).ready(function() {
+	
+	setTimeout(function(){
+		$('body').addClass('loaded');
+		$('h1').css('color','#222222');
+	}, 3000);
+	
+});
